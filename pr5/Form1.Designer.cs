3c615ca0040e4c8f46a593fe4c48d641d4ca8eff
Universai <solomatin377@gmail.com>
2020-09-21 11:28:47 +0300

    dd

--- pr5/Form1.Designer.cs
+++ pr5/Form1.Designer.cs
@@ -29,31 +29,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(602, 294);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(74, 20);
-            this.textBox1.TabIndex = 0;
+            this.label1.Location = new System.Drawing.Point(688, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
-            this.PerformLayout();
         }
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
 
         #endregion
     }
--- pr5/Form1.cs
+++ pr5/Form1.cs
@@ -13,11 +13,12 @@ namespace pr5
 {
     public partial class Form1 : Form
     {
-        // ReSharper disable once CollectionNeverUpdated.Local
         // ReSharper disable once FieldCanBeMadeReadOnly.Local
-        // ReSharper disable once InconsistentNaming
-        // ReSharper disable once IdentifierTypo
-        List<Shape> SPLIST = new List<Shape>();
+        List<Shape> _splist = new List<Shape>(); //shapes
+
+        bool _mousePressed = true;
+        List<(int i, int dx, int dy)> _pindex = new List<(int i, int dx, int dy)>(); //pressed shapes
+
         public Form1()
         {
             InitializeComponent();
@@ -25,26 +26,64 @@ namespace pr5
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            switch (e.Button)
             {
-                foreach (var sp in SPLIST) if (sp.IsInside(e.X, e.Y)) return;
-                SPLIST.Add(new Circle(e.X, e.Y));
-                Invalidate();
-            }
-            else if (e.Button == MouseButtons.Right)
-            {
-                for (int i = 0; i < SPLIST.Count; i++)
-                    if (SPLIST[i].IsInside(e.X, e.Y))
-                        SPLIST.RemoveAt(i);
-                Invalidate();
+                case MouseButtons.Left:
+                    foreach (var sp in _splist)
+                        if (sp.IsInside(e.X, e.Y))
+                            return;
+                    _splist.Add(new Circle(e.X, e.Y));
+                    Invalidate();
+                    break;
+
+                case MouseButtons.Right:
+                    for (int i = _splist.Count - 1; i >= 0; i--)
+                        if (_splist[i].IsInside(e.X, e.Y))
+                        {
+                            _splist.RemoveAt(i);
+                            break;
+                        }
+
+                    Invalidate();
+                    break;
             }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            foreach (var sp in SPLIST)
+            foreach (var sp in _splist)
             {
                 sp.Draw(e.Graphics);
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _mousePressed = true;
+                for (int i = 0; i < _splist.Count; i++)
+                    if (_splist[i].IsInside(e.X, e.Y))
+                        _pindex.Add((i, e.X - _splist[i].X, e.Y - _splist[i].Y));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _mousePressed = false;
+            _pindex = new List<(int i, int dx, int dy)>();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mousePressed && _pindex.Count != 0)
+            {
+                foreach (var el in _pindex)
+                {
+                    (_splist[el.i].X, _splist[el.i].Y) = (e.X - el.dx, e.Y - el.dy);
+                }
+
+                Invalidate();
             }
         }
     }
